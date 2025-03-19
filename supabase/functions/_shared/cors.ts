7bcc8d3537f9a42c3104547e4ad48f608da8c7db
Richard Annilo <richard.annilo@gmail.com>
2025-03-19 13:06:21 +0200

    booking confirmation email (wip)

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,5 +1,7 @@
+// CORS headers for Supabase edge functions
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-timezone',
-}
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-supabase-auth, Accept',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Max-Age': '86400'
+};
